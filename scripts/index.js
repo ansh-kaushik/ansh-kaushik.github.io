661b30d923c7a6729e18fb9f8960f4ecd16e6da0
ANSH KAUSHIK <anshkaushik9454@gmail.com>
2022-12-23 11:53:46 +0530

    nav-bar created

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+const mainNav = document.querySelector('.main-nav');
+document.addEventListener('click', (e) => {
+  if (e.target.matches('.main-nav')) {
+  } else if (e.target.matches('.toggle-button')) {
+    mainNav.classList.toggle('active');
+  } else {
+    mainNav.classList.remove('active');
+  }
+});
