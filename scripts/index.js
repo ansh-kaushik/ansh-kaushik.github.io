281e6f7e2b873044fba18369781d3c6c4f65afb6
ANSH KAUSHIK <anshkaushik9454@gmail.com>
2022-12-24 10:06:51 +0530

    commit: 'updated'

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,10 @@
 const mainNav = document.querySelector('.main-nav');
+const mailForm = document.querySelector('.mail-form');
+const name = document.querySelector('.name');
+const email = document.querySelector('.email');
+const message = document.querySelector('.message');
+
+// for side nav bar
 document.addEventListener('click', (e) => {
   if (e.target.matches('.main-nav')) {
   } else if (e.target.matches('.toggle-button')) {
